6e42f811fa2c7d6e1e747795811c8f75eebb3113
Sho Emoto <sho.emoto@gmail.com>
2021-07-22 18:18:21 +0900

    リファクタリング

--- flutter_app_original_sample/lib/page/MyRootPage.dart
+++ flutter_app_original_sample/lib/page/MyRootPage.dart
@@ -78,87 +78,7 @@ class _MyRootPageState extends State<MyRootPage> with SingleTickerProviderStateM
 
   Widget _createTab(Tab tab) {
     if (tab.text == 'One') {
-      return NotificationListener(
-        onNotification: (scrollNotification) {
-          if (controller.page == 1) {
-            return true;
-          }
-          if (scrollNotification is ScrollEndNotification) {
-            final scrollProportion =
-                scrollNotification.metrics.pixels / scrollNotification.metrics.maxScrollExtent;
-
-            if (scrollProportion >= 1.0) {
-              controller.requestHttp();
-            }
-          }
-          return false;
-        },
-          child: RefreshIndicator(
-            onRefresh: _onRefresh,
-            child: Obx(() => Scaffold(
-              body: ListView.separated(
-                separatorBuilder: (BuildContext context, int index) => Divider(color: Colors.blueGrey,),
-                itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                    leading: Icon(Icons.flutter_dash),
-                    title: Text('No.$index: ' + controller.list[index].title),
-                    onTap: () async {
-                      var result = await showDialog<String>(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              title: Text('No.$index'),
-                              content: Text(controller.list[index].title),
-                              actions: [
-                                TextButton(
-                                    onPressed: () => Navigator.of(context).pop('Cancel: ' + controller.list[index].title),
-                                    child: Text('Cancel')
-                                ),
-                                TextButton(
-                                    onPressed: () => Navigator.of(context).pop('OK: ' + controller.list[index].title),
-                                    child: Text('OK')
-                                )
-                              ],
-                            );
-                          },
-                        barrierDismissible: false,
-                      );
-                      print(result);
-                    },
-                    onLongPress: () async {
-                      var result = await showDialog<String>(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return SimpleDialog(
-                            title: Text('No.$index: ' + controller.list[index].title),
-                            children: [
-                              SimpleDialogOption(
-                                  onPressed: () => Navigator.of(context).pop('select Option1'),
-                                  child: Text('Option1')
-                              ),
-                              SimpleDialogOption(
-                                  onPressed: () => Navigator.of(context).pop('select Option2'),
-                                  child: Text('Option2')
-                              ),
-                              SimpleDialogOption(
-                                  onPressed: () => Navigator.of(context).pop('select Option3'),
-                                  child: Text('Option3')
-                              )
-                            ],
-                          );
-                        },
-                        barrierDismissible: false,
-                      );
-                      print(result);
-                    },
-                  );
-                },
-                itemCount: controller.list.length,
-              ),
-            )
-            ),
-          )
-      );
+      return _createFirstTab();
     } else if(tab.text == 'Two') {
       return Center(
         child: TextButton(
@@ -179,5 +99,89 @@ class _MyRootPageState extends State<MyRootPage> with SingleTickerProviderStateM
 
   Future<void> _onRefresh() async {
     controller.refresh();
+  }
+
+  Widget _createFirstTab() {
+    return NotificationListener(
+        onNotification: (scrollNotification) {
+          if (controller.page == 1) {
+            return true;
+          }
+          if (scrollNotification is ScrollEndNotification) {
+            final scrollProportion =
+                scrollNotification.metrics.pixels / scrollNotification.metrics.maxScrollExtent;
+
+            if (scrollProportion >= 1.0) {
+              controller.requestHttp();
+            }
+          }
+          return false;
+        },
+        child: RefreshIndicator(
+          onRefresh: _onRefresh,
+          child: Obx(() => Scaffold(
+            body: ListView.separated(
+              separatorBuilder: (BuildContext context, int index) => Divider(color: Colors.blueGrey,),
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  leading: Icon(Icons.flutter_dash),
+                  title: Text('No.$index: ' + controller.list[index].title),
+                  onTap: () async {
+                    var result = await showDialog<String>(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text('No.$index'),
+                          content: Text(controller.list[index].title),
+                          actions: [
+                            TextButton(
+                                onPressed: () => Navigator.of(context).pop('Cancel: ' + controller.list[index].title),
+                                child: Text('Cancel')
+                            ),
+                            TextButton(
+                                onPressed: () => Navigator.of(context).pop('OK: ' + controller.list[index].title),
+                                child: Text('OK')
+                            )
+                          ],
+                        );
+                      },
+                      barrierDismissible: false,
+                    );
+                    print(result);
+                  },
+                  onLongPress: () async {
+                    var result = await showDialog<String>(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return SimpleDialog(
+                          title: Text('No.$index: ' + controller.list[index].title),
+                          children: [
+                            SimpleDialogOption(
+                                onPressed: () => Navigator.of(context).pop('select Option1'),
+                                child: Text('Option1')
+                            ),
+                            SimpleDialogOption(
+                                onPressed: () => Navigator.of(context).pop('select Option2'),
+                                child: Text('Option2')
+                            ),
+                            SimpleDialogOption(
+                                onPressed: () => Navigator.of(context).pop('select Option3'),
+                                child: Text('Option3')
+                            )
+                          ],
+                        );
+                      },
+                      barrierDismissible: false,
+                    );
+                    print(result);
+                  },
+                );
+              },
+              itemCount: controller.list.length,
+            ),
+          )
+          ),
+        )
+    );
   }
 }
