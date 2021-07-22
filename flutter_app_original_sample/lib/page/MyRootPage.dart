c907d5748225c08d503f133d79c921d42f3aaaa6
Sho Emoto <sho.emoto@gmail.com>
2021-07-22 19:28:38 +0900

    リファクタリング

--- flutter_app_original_sample/lib/page/MyRootPage.dart
+++ flutter_app_original_sample/lib/page/MyRootPage.dart
@@ -92,9 +92,9 @@ class _MyRootPageState extends State<MyRootPage> with SingleTickerProviderStateM
       return Center(
           child: Column(
             children: [
-              Image.asset('images/apple.png'),
+              Image.asset('assets/images/apple.png'),
               FadeInImage.assetNetwork(
-                placeholder: 'images/placeholder.png',
+                placeholder: 'assets/images/placeholder.png',
                 image: 'https://1.bp.blogspot.com/-ZOg0qAG4ewU/Xub_uw6q0DI/AAAAAAABZio/MshyuVBpHUgaOKJtL47LmVkCf5Vge6MQQCNcBGAsYHQ/s1600/pose_pien_uruuru_woman.png',
               ),
             ],
